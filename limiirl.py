ba8f1001cdc34872cc28f0c75656d3de21c176cc
isabelkim <isabel_kim@college.harvard.edu>
2023-11-04 17:39:49 -0400

    LIMIIRL rho u

--- limiirl.py
+++ limiirl.py
@@ -4,30 +4,46 @@ from sklearn.mixture import GaussianMixture
 from sklearn.mixture import BayesianGaussianMixture
 from sklearn.metrics import pairwise_distances_argmin_min
 
-# Feature representation of demonstrations (X)
+def limiirl(taus, K=1000):
+    # Feature representation of demonstrations (X)
 
 
-# Number of clusters or behavior modes (K)
+    # Number of clusters or behavior modes (K)
 
+    # Warm-start: Cluster the demonstrations using K-Means to initialize EM.
+    kmeans = KMeans(n_clusters=K)
+    cluster_model = kmeans.fit(taus)
 
-# Warm-start: Cluster the demonstrations using K-Means to initialize EM.
-kmeans = KMeans(n_clusters=K)
-cluster_assignments = kmeans.fit_predict(X)
+    # EM (Expectation Maximization)
+    # Initialize reward function parameters.
+    n = len(taus) 
+    u = np.zeros((n, K))
 
-# EM (Expectation Maximization)
-# Initialize reward function parameters.
-theta = np.random.rand(K, num_features)
-
-# Define the EM loop.
-max_iterations = 100
-for iteration in range(max_iterations):
-    # E-step: Assign demonstrations to clusters.
-    cluster_assignments, _ = pairwise_distances_argmin_min(X, theta)
-    
-    # M-step: Update the reward function parameters (theta).
+    for i in range(n):
+        for j in range(K):
+            c = cluster_model.predict(taus[i])
+            u[i][j] = 1 if c == j else 0
+                
+    rho = np.zeros(K)
     for k in range(K):
-        mask = (cluster_assignments == k)
-        if np.sum(mask) > 0:
-            theta[k] = np.mean(X[mask], axis=0)
+        sum = 0
+        for i in range(n):
+            sum += u[i][k]
+        rho[k] = sum / n
 
-# After EM converges, theta should contain reward functions for each cluster.
+
+    theta = np.random.rand(K, num_features)
+
+    # Define the EM loop.
+    max_iterations = 100
+    for iteration in range(max_iterations):
+        # E-step: Assign demonstrations to clusters.
+        cluster_assignments, _ = pairwise_distances_argmin_min(taus, theta)
+        
+        # M-step: Update the reward function parameters (theta).
+        for k in range(K):
+            mask = (cluster_assignments == k)
+            if np.sum(mask) > 0:
+                theta[k] = np.mean(taus[mask], axis=0)
+
+    # After EM converges, theta should contain reward functions for each cluster.
